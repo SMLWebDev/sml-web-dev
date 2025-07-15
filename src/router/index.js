f668f5ed630106e0b1617877a7eef2570dd16b9f
Simon <simon@smlwebdevelopment.co.uk>
2025-07-15 15:41:53 +0100

    Added vue-router for Vercel Analytics

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '../App.vue'
+
+export default createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: App,
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            redirect: '/'
+        }
+    ]
+})
